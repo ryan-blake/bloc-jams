6e0cacf98b335a294778c0fb991fcba170a1bb9a
Ryan Kliemann <Ryan_blake@icloud.com>
2016-03-23 18:35:00 -0500

    check -23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
+var animatePoints = function(points) {
   var revealPoint = function(index) {
     points[index].style.opacity = 0.9;
     points[index].style.transform = "scaleX(1) translateY(0)";
@@ -10,7 +10,21 @@ var animatePoints = function() {
   }
   for (var i = 0; i < points.length; i++) {
     revealPoint(i);
-
   }
 
 };
+
+ window.onload = function() {
+   // auto animate for tall screens
+   if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+}
+   var sellingPoints = document.getElementsByClassName('selling-points')[0];
+   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+   window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+           animatePoints(pointsArray);
+    }
+   });
+}
