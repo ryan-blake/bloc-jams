5e3c4255e4dfa4eb0cd1133fb905e097c1471458
Ryan Kliemann <Ryan_blake@icloud.com>
2016-03-25 13:33:13 -0500

    updatelanding.js-nxt.collections

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,6 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
-<<<<<<< HEAD
-
-var revealPoint = function(index) {
-              points[index].style.opacity = 1;
-              points[index].style.transform = "scaleX(1) translateY(0)";
-              points[index].style.msTransform = "scaleX(1) translateY(0)";
-              points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-          }
-
-    for (var i = 0; i < points.length; i++){
-      revealPoint(i);
-    }
-  };
-      window.onload = function() {
-        var sellingPoints = document.getElementsByClassName('selling-points')[0];
-
-        window.addEventListener('scroll', function(event) {
-          var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-                      animatePoints(pointsArray);
-                  }
-           });
-        }
-=======
   var revealPoint = function(index) {
     points[index].style.opacity = 0.9;
     points[index].style.transform = "scaleX(1) translateY(0)";
@@ -53,4 +28,3 @@ var revealPoint = function(index) {
     }
    });
 }
->>>>>>> check_26
