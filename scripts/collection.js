2b4a0a34347553a35ea8b7281add1a94e1be963e
Ryan Kliemann <Ryan_blake@icloud.com>
2016-04-13 11:03:09 -0500

    refactoring with jquery in collection and album pages.

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,6 @@
 // album template
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/03.png"/>'
   + '  <div class="collection-album-info caption">'
@@ -14,15 +15,20 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
+
+  return $(template);
+};
 // add albums to collection page with template variable
-  window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
+$(window).load(function() {
+    // #3
+    var $collectionContainer = $('.album-covers');
+    // #4
+    $collectionContainer.empty();
 
     // #3
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+      var $newThumbnail = buildCollectionItemTemplate();
+      $collectionContainer.append($newThumbnail);
+
     }
-}
+});
