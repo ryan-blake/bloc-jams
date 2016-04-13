cffdad03847cae7dcc3bab8c7447569e777a85dd
Ryan Kliemann <Ryan_blake@icloud.com>
2016-04-13 17:08:36 -0500

    merge32

--- scripts/album.js
+++ scripts/album.js
@@ -152,7 +152,5 @@ var noParent = document.querySelector('html');
   var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
   var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-// Store state of playing songs
-
 
 });
